e4ec3197b0a79f03919b93449c071b1fdb8d26c4
Luca Garizio <garizioluca2@gmail.com>
2023-11-23 15:52:55 +0100

    feat: add logic on eventlistener (ID next)

--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@
 // Al click dell’utente sulle frecce (sia Prev che Next),
 // il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 
+
+const item = document.getElementsByClassName("item");
+console.log(item[0]);
+
+let activeItems = 0;
+
 const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+next.addEventListener("click",
+
+    function(){
+        if (activeItems < item.length - 1)
+        item[activeItems].classList.remove("active");
+
+        activeItems++;
+
+        item[activeItems].classList.add("active");
+    }   
+)
+
