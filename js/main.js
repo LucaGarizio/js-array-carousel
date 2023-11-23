9ba1a78aa474e2bf687bcf2a9486940b8c3c7d4d
Luca Garizio <garizioluca2@gmail.com>
2023-11-23 15:35:47 +0100

    fix: remove carousel-container

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 // MILESTONE 2
 // Al click dell’utente sulle frecce (sia Prev che Next),
 // il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+const next = document.getElementById("next");
