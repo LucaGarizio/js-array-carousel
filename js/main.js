efaeeae439eac51e5a36030edfa5413f1d7a1e6d
Luca Garizio <garizioluca2@gmail.com>
2023-11-23 16:24:17 +0100

    style: add comment on java

--- js/main.js
+++ js/main.js
@@ -3,40 +3,42 @@
 // il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 
 
+// prendere tutti gli elementi sotto la classe item e salvarli in una variabile
 const item = document.getElementsByClassName("item");
 console.log(item[0]);
-
+// variabile con valore iniziale 0
 let activeItems = 0;
-
+// creazione variabile per referenza
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
+
 
 next.addEventListener("click",
 
     function(){
-
-        
-        if (activeItems < item.length - 1)
+        // controllare se ci sono altre immagini da mostrare
+        if (activeItems < item.length -1)
+        // rimuovere la classe attivo dalla foto attuale
         item[activeItems].classList.remove("active");
-
+        // aumenta l'index di 1
         activeItems++;
-
+        // aggiungere la classe active alla foto successiva 
         item[activeItems].classList.add("active");
     }   
 )
 
 
-
 prev.addEventListener("click",
 
     function(){
+        // controllare se ci sono altre immagini precedenti da mostrare
         if (activeItems > 0){
-
-            item[activeItems].classList.remove("active");
-
-            activeItems--;
-
-            item[activeItems].classList.add("active");
+        // rimuovere la classe attivo dalla foto attuale
+        item[activeItems].classList.remove("active");
+        // diminuisci l'index di 1
+        activeItems--;
+        // aggiungere la classe active alla foto precedente 
+        item[activeItems].classList.add("active");
         }
     }
 )
